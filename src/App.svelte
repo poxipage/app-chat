4002fb3452a804ea7f80a1aaec6175b578868226
Filip Tangen <filiptangen1998@live.com>
2022-09-15 12:06:13 +0200

    init commit app chat

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import { getState } from "./state";
+  const state = getState();
+</script>
+
+<div class="h-full w-full flex justify-center items-center">
+  <button
+    class="btn btn-primary"
+    on:click={() => {
+      $state.count += 1;
+    }}
+  >
+    {$state.count}
+  </button>
+</div>
